0bbff47b3674f95ba2e1a856953d69b648a5e735
Paula Santana <psanrosa13@gmail.com>
2020-05-01 15:54:18 -0300

    Criando servidor socket usando threads para cada conexão com clientes socket

---
+++ src/server/DistribuirTarefas.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.net.Socket;
+
+public class DistribuirTarefas implements Runnable{
+	
+	 private Socket socket;
+
+	    public DistribuirTarefas(Socket socket) {
+	        this.socket = socket;
+	    }
+
+	    @Override
+	    public void run() {
+	        System.out.println("Distribuindo as tarefas para o cliente " +  socket);
+
+	        try {
+	            Thread.sleep(20000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+}
---
+++ src/server/ServidorTarefas.java
@@ -0,0 +1,20 @@
+package server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTarefas {
+	public static void main(String[] args) throws Exception {
+		System.out.println("---- Iniciando Servidor ----");
+	    ServerSocket servidor = new ServerSocket(12345);
+
+	    while (true) {
+            Socket socket = servidor.accept();    
+            System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+            
+            DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
+            
+            new Thread(distribuirTarefas).start();
+        }
+    }
+}
